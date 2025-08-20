f37a4c93a078641a3d7af965e01e5323155e8c9d
Pierre-Louis Suckrow <pierresuckrow@tutanota.com>
2025-08-20 14:53:20 +0200

    wip setup

--- main.py
+++ main.py
@@ -9,13 +9,40 @@ load_dotenv(find_dotenv())
 from sentence_transformers import SentenceTransformer
 from mythologizer_core import setup_simulation
 from mythologizer_core.types import Embedding
+from mythologizer_core import AgentAttribute
 
 def standard_embedding_function(text: str) -> Embedding:
     model = SentenceTransformer("all-MiniLM-L6-v2")
     return model.encode(text)
 
 def main():
-    setup_simulation(standard_embedding_function)
+    mythemes = ["The Hero's Journey", "Another Mytheme"]
+
+    agent_attributes = [
+    AgentAttribute(
+        name='Age',
+        description='Age of the agent',
+        d_type=int,
+        min=0,
+        epoch_change_function= lambda x: x + 1
+    ),
+    AgentAttribute(
+        name='Confidence',
+        description='The confidence of the agent',
+        d_type=float,
+        min=0.0,
+        max=1.0,
+        epoch_change_function= lambda x: x + random.random()
+    ),
+    AgentAttribute(
+        name='Emotionality',
+        description='The emotionality of the agent with 0 representing a very emotionless person and 1 representing a very emotional person',
+        d_type=float,
+        min=0.0,
+        max=1.0)
+    ]
+
+    setup_simulation(standard_embedding_function, mythemes, agent_attributes)
 
 if __name__ == "__main__":
     main()
--- mythologizer_core/setup.py
+++ mythologizer_core/setup.py
@@ -1,6 +1,9 @@
 from mythologizer_core.types import EmbeddingFunction
 from mythologizer_core import AgentAttribute
-from mythologizer_postgres.db import apply_schemas, drop_all_tables
+from mythologizer_postgres.db import apply_schemas, drop_everything
+from mythologizer_postgres.connectors.mytheme_store import insert_mythemes_bulk
+from mythologizer_postgres.connectors import insert_agent_attribute_defs
+
 import logging
 import numpy as np
 
@@ -16,15 +19,31 @@ def get_embedding_dim(embedding_function: EmbeddingFunction) -> int:
             raise ValueError(f"Unsupported embedding type: {type(test_embedding)}")
         return embedding_dim
 
-def setup_simulation(embedding_function: EmbeddingFunction) -> None:
+
+def setup_simulation(embedding_function: EmbeddingFunction, mythemes: list[str], agent_attributes: list[AgentAttribute]) -> None:
     
     embedding_dim = get_embedding_dim(embedding_function)
     logger.info(f"Embedding dimension: {embedding_dim}")
 
-    drop_all_tables()
+    drop_everything()
     logger.info("Dropped all tables")
     apply_schemas(embedding_dim)
     logger.info(f"Applied schemas with embedding dimension: {embedding_dim}")
+
+    logger.info("Computing mytheme embeddings")
+    mytheme_embeddings = [embedding_function(mytheme) for mytheme in mythemes]
+    
+    logger.info("Inserting mytheme embeddings into database")
+    insert_mythemes_bulk(mythemes,mytheme_embeddings)
+
+    logger.info("Inserting agent attribute definitions into database")
+    insert_agent_attribute_defs(agent_attributes)
+
+    logger.info("Finished")
+
+
+
+
 
 
 
